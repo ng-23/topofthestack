36f30809eb34ca8b3c912a618ba49e79a040103d
= <=>
2023-06-06 14:02:00 -0400

    Formatting

--- src/core/model/implementations/entities/users/UserFactory.php
+++ src/core/model/implementations/entities/users/UserFactory.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 // consider moving this into utils or even services (?)
 require_once("User.php");
 
-class UserFactory {
+class UserFactory
+{
     public const DEFAULT_PASSWORD = "password12345";
 
-    public function makeUser(String $email): User {
+    public function makeUser(String $email): User
+    {
         $hash = password_hash(self::DEFAULT_PASSWORD, PASSWORD_BCRYPT);
         return new User($email, $hash);
     }
-
-
 }
