ca89091eaea0010c1b1b6d0ca9bce7b0a23029cf
ng-23 <23grattan@gmail.com>
2023-06-11 19:51:10 -0400

    Minor changes Added hours, minutes, and seconds to DATE_FORMAT constant to reflect changes in storing of created_at and updated_at. Formatting

--- src/core/model/implementations/entities/tags/Tag.php
+++ src/core/model/implementations/entities/tags/Tag.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 require_once realpath(dirname(__FILE__) . '../../../../') . '/abstractions/entities/Entity.php';
 
-class Tag extends Entity{
-    public const DATE_FORMAT = "Y-m-d";
+class Tag extends Entity
+{
+    public const DATE_FORMAT = "Y-m-d H:i:s";
     public const MIN_NAME_LEN = 3;
     public const MAX_NAME_LEN = 15;
     public const VALID_NAME_REGEX = "#^[0-9a-zA-Z_.-]{" . self::MIN_NAME_LEN . "," . self::MAX_NAME_LEN . "}$#";
@@ -15,7 +16,8 @@ class Tag extends Entity{
     private int $tagged_with_today;
     private DateTimeImmutable $created_at, $updated_at;
 
-    public function __construct(String $name, ?int $tag_id = NULL) {
+    public function __construct(String $name, ?int $tag_id = NULL)
+    {
         $this->id = $tag_id;
         $this->total_tagged_with = $this->tagged_with_today = 0;
         $this->setName($name);
@@ -23,78 +25,86 @@ class Tag extends Entity{
         $this->updated_at = $this->setUpdatedAt(NULL);
     }
 
-    public function getName(): String {
+    public function getName(): String
+    {
         return $this->name;
     }
 
-    public function getTotalTaggedWith(): int {
+    public function getTotalTaggedWith(): int
+    {
         return $this->total_tagged_with;
     }
 
-    public function getTaggedWithToday(): int {
+    public function getTaggedWithToday(): int
+    {
         return $this->tagged_with_today;
     }
 
-    public function getCreatedAt(): DateTimeImmutable {
+    public function getCreatedAt(): DateTimeImmutable
+    {
         return $this->created_at;
     }
 
-    public function getUpdatedAt(): DateTimeImmutable {
+    public function getUpdatedAt(): DateTimeImmutable
+    {
         return $this->updated_at;
     }
 
-    public function setId(?int $tag_id) {
+    public function setId(?int $tag_id)
+    {
         $this->id = $tag_id;
     }
 
-    public function setName(String $name) {
+    public function setName(String $name)
+    {
         $is_valid = preg_match(self::VALID_NAME_REGEX, $name);
-        if(!$is_valid) {
+        if (!$is_valid) {
             throw new Exception();
         }
 
         $this->name = $name;
     }
 
-    public function setTotalTaggedWith(int $tagged_with) {
-        if($tagged_with < 0) {
+    public function setTotalTaggedWith(int $tagged_with)
+    {
+        if ($tagged_with < 0) {
             throw new Exception();
         }
         $this->total_tagged_with = 0;
     }
 
-    public function setTaggedWithToday(int $tagged_with) {
-        if($tagged_with < 0) {
+    public function setTaggedWithToday(int $tagged_with)
+    {
+        if ($tagged_with < 0) {
             throw new Exception();
         }
         $this->tagged_with_today = 0;
     }
 
-    public function setCreatedAt(?DateTimeImmutable $date) {
-        if($date) {
+    public function setCreatedAt(?DateTimeImmutable $date)
+    {
+        if ($date) {
             $this->created_at = $date;
-        }
-
-        else {
+        } else {
             $this->created_at = new DateTimeImmutable("now");
         }
     }
 
-    public function setUpdatedAt(?DateTimeImmutable $date) {
-        if($date) {
+    public function setUpdatedAt(?DateTimeImmutable $date)
+    {
+        if ($date) {
             $this->updated_at = $date;
-        }
-
-        else {
+        } else {
             $this->updated_at = DateTimeImmutable::createFromFormat(self::DATE_FORMAT, $this->created_at->format(self::DATE_FORMAT));
         }
     }
 
-    public function toArray(): array {
+    public function toArray(): array
+    {
         $reflect = new ReflectionClass($this);
         $properties = $reflect->getProperties();
         $data = [];
-        foreach($properties as $property) {
+        foreach ($properties as $property) {
             $property_name = $property->getName();
             $data[$property_name] = $this->$property_name;
         }
