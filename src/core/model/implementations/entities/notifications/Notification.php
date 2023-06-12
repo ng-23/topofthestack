33695245098f02e8994fd51713e19e9f4140634e
ng-23 <23grattan@gmail.com>
2023-06-11 21:58:50 -0400

    Notification types and smaller changes Added constants to indicate the "type" of notification. Added getter and setter for notification type. Unprivated constants and added DATE_FORMAT constant. Formatting.

--- src/core/model/implementations/entities/notifications/Notification.php
+++ src/core/model/implementations/entities/notifications/Notification.php
@@ -4,94 +4,138 @@ declare(strict_types=1);
 
 require_once realpath(dirname(__FILE__) . '../../../../') . '/abstractions/entities/Entity.php';
 
-class Notification extends Entity {
+class Notification extends Entity
+{
+    public const VALID_TYPES = [
+        "TYPE_SYSTEM" => 0,
+        "TYPE_BLOG_COMMENT" => 1,
+        "TYPE_FOLLOW_REQ" => 2,
+        "TYPE_BLOG_FROM_FOLLOWING" => 3,
+    ];
+
+    public const DATE_FORMAT = "Y-m-d H:i:s";
+    public const MAX_HEADER_LEN = 64;
+    public const MAX_BODY_LEN = 120;
+    public const MAX_URI_LEN = 255;
+
     private int $user_id;
+    private int $type;
     private String $header, $body;
     private ?String $image_uri;
-    private int $seen;
-    private int $created_at;
+    private bool $seen;
+    private DateTimeImmutable $created_at;
 
-    private const MAX_HEADER_LEN = 64;
-    private const MAX_BODY_LEN = 120;
-    private const MAX_URI_LEN = 255;
-
-    public function __construct(int $user_id, String $header, String $body, ?int $notification_id = NULL) {
+    public function __construct(int $user_id, int $type, String $header, String $body, ?int $notification_id = NULL)
+    {
         $this->id = $notification_id;
+        $this->setType($type);
         $this->user_id = $user_id;
         $this->header = $this->setHeader($header);
         $this->body = $this->setBody($body);
-        $this->seen = 0;
+        $this->seen = false;
         $this->created_at = time();
         $this->image_uri = NULL;
     }
 
-    public function getUserId(): int {
+    public function getUserId(): int
+    {
         return $this->user_id;
     }
 
-    public function getHeader(): String {
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    public function getHeader(): String
+    {
         return $this->header;
     }
 
-    public function getBody(): String {
+    public function getBody(): String
+    {
         return $this->body;
     }
 
-    public function getImageUri(): String {
+    public function getImageUri(): String
+    {
         return $this->image_uri;
     }
 
-    public function wasSeen(): int {
+    public function wasSeen(): bool
+    {
         return $this->seen;
     }
 
-    public function getCreatedAt(): int {
+    public function getCreatedAt(): DateTimeImmutable
+    {
         return $this->created_at;
     }
-    
-    public function setSeen(int $seen = 0) {
-        if($seen < 0 or $seen > 1) {
+
+    public function setId(?int $notification_id)
+    {
+        $this->id = $notification_id;
+    }
+
+    public function setType(int $type)
+    {
+        $is_valid = false;
+
+        foreach (self::VALID_TYPES as $valid_type) {
+            if ($type == $valid_type) {
+                $this->type = $type;
+                $is_valid = true;
+                break;
+            }
+        }
+
+        if (!$is_valid) {
             throw new Exception();
         }
+    }
+
+    public function setSeen(bool $seen)
+    {
         $this->seen = $seen;
     }
 
-    public function setImageUri(?String $uri = NULL) {
-        if($uri) {
+    public function setImageUri(?String $uri)
+    {
+        if ($uri) {
             $uri_len = strlen($uri);
-            if($uri_len == 0 or $uri_len > self::MAX_URI_LEN) {
+            if ($uri_len == 0 or $uri_len > self::MAX_URI_LEN) {
                 throw new Exception();
             }
         }
-        
+
         $this->image_uri = $uri;
     }
 
-    public function setHeader(String $header) {
+    public function setHeader(String $header)
+    {
         $header_len = strlen($header);
-        if($header_len == 0 or $header_len > self::MAX_HEADER_LEN) {
+        if ($header_len == 0 or $header_len > self::MAX_HEADER_LEN) {
             throw new Exception();
         }
         $this->header = $header;
     }
 
-    public function setBody(String $body) {
+    public function setBody(String $body)
+    {
         $body_len = strlen($body);
-        if($body_len == 0 or $body_len > self::MAX_BODY_LEN) {
+        if ($body_len == 0 or $body_len > self::MAX_BODY_LEN) {
             throw new Exception();
         }
+
         $this->body = $body;
     }
 
-    public function setCreatedAt(?int $unix_timestamp = NULL) {
-        if($unix_timestamp) {
-            $this->created_at = $unix_timestamp;
-        }
-        else {
-            $this->created_at = time();
+    public function setCreatedAt(?DateTimeImmutable $date)
+    {
+        if ($date) {
+            $this->created_at = $date;
+        } else {
+            $this->created_at = new DateTimeImmutable("now");
         }
     }
-
-    
-    
 }
