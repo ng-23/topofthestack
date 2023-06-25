8b0b56ca8031ead1246a0a49c5a6ad5904c01adb
ng-23 <23grattan@gmail.com>
2023-06-25 11:20:50 -0400

    Image handling Changed image_uri setter. Now has ability to set a default image for notifications. Added image_data field and setter. Added toArray method.

--- src/core/model/implementations/entities/notifications/Notification.php
+++ src/core/model/implementations/entities/notifications/Notification.php
@@ -3,9 +3,14 @@
 declare(strict_types=1);
 
 require_once realpath(dirname(__FILE__) . '../../../../') . '/abstractions/entities/Entity.php';
+require_once realpath(dirname(__FILE__) . '../../../../../') . '/utils/is_image.php';
 
 class Notification extends Entity
 {
+    /* public const TYPE_SYSTEM = 0;
+    public const TYPE_BLOG_COMMENT = 1;
+    public const TYPE_FOLLOW_REQ = 2;
+    public const TYPE_BLOG_FROM_FOLLOWING = 3; */
     public const VALID_TYPES = [
         "TYPE_SYSTEM" => 0,
         "TYPE_BLOG_COMMENT" => 1,
@@ -13,15 +18,31 @@ class Notification extends Entity
         "TYPE_BLOG_FROM_FOLLOWING" => 3,
     ];
 
+    // consider making this a timestamp for more precise record keeping
+    // maybe make everything a timestamp
+    // although for i don't really care to know when exactly a user signed up...
+    // also run in to the issue of best way to store the timestamps, as the timestamp data type in sql only goes up to 2038
+    // could store as a unix big int timestamp in the DB, then convert to a datetimeimmutable object in php
+    // of coure, this makes it so you have to manually change the updated_at timestamp
     public const DATE_FORMAT = "Y-m-d H:i:s";
     public const MAX_HEADER_LEN = 64;
-    public const MAX_BODY_LEN = 120;
-    public const MAX_URI_LEN = 255;
+    public const MAX_BODY_LEN = 150;
+    public const MIN_IMG_URI_LEN = 5;
+    public const MAX_IMG_URI_LEN = 255;
 
     private int $user_id;
+    /**
+     * on SO, header would be equivalent to something like "question closed"
+     * and body would be like "your question `Really Dumb Question` was recently close"
+     * if the mapper is going to check if a notification is allowed to be sent to a user, it needs to know the
+     * notifications type and then check the user's settings to see if that type is disallowed
+     * so introduce a type variable in here (and of course the DB)
+     * it should be a numerical code, where (for example) 1 = comment on post, 2 = follow request, 3 = blog from following, etc.
+     * maybe also change header and body to header_text and body_text or something like that (for clarity sake)
+     */
     private int $type;
     private String $header, $body;
-    private ?String $image_uri;
+    private String $image_uri, $image_data;
     private bool $seen;
     private DateTimeImmutable $created_at;
 
@@ -33,8 +54,8 @@ class Notification extends Entity
         $this->header = $this->setHeader($header);
         $this->body = $this->setBody($body);
         $this->seen = false;
-        $this->created_at = time();
-        $this->image_uri = NULL;
+        $this->setCreatedAt(NULL);
+        $this->setImageUri(NULL);
     }
 
     public function getUserId(): int
@@ -60,6 +81,11 @@ class Notification extends Entity
     public function getImageUri(): String
     {
         return $this->image_uri;
+    }
+
+    public function getImageData(): String
+    {
+        return $this->image_data;
     }
 
     public function wasSeen(): bool
@@ -103,12 +129,35 @@ class Notification extends Entity
     {
         if ($uri) {
             $uri_len = strlen($uri);
-            if ($uri_len == 0 or $uri_len > self::MAX_URI_LEN) {
+            if ($uri_len < self::MIN_IMG_URI_LEN or $uri_len > self::MAX_IMG_URI_LEN) {
                 throw new Exception();
             }
+            $this->image_uri = $uri;
+        } else {
+            $this->image_uri = NotificationMapper::IMG_DIR . "/logo_small1.png";
+
+            $this->setImageData(file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/{$this->image_uri}"));
+        }
+    }
+
+    /**
+     * bug: someone could set the default uri
+     * then overwrite its image data and save
+     * need to prevent that somehow
+     * bug exists in other classes with image URI stuff, but fix it later...
+     */
+    public function setImageData(String $image_data)
+    {
+        $size_in_bytes = strlen($image_data);
+        if ($size_in_bytes == 0 or $size_in_bytes > UserMapper::PFP_MAX_FILE_SIZE_MB * User::BYTES_IN_MB) {
+            throw new Exception();
         }
 
-        $this->image_uri = $uri;
+        if (!is_png($image_data) or !is_jpg($image_data)) {
+            throw new Exception();
+        }
+
+        $this->image_data = $image_data;
     }
 
     public function setHeader(String $header)
@@ -137,5 +186,17 @@ class Notification extends Entity
         } else {
             $this->created_at = new DateTimeImmutable("now");
         }
+    }
+
+    public function toArray(): array
+    {
+        $reflect = new ReflectionClass($this);
+        $properties = $reflect->getProperties();
+        $data = [];
+        foreach ($properties as $property) {
+            $property_name = $property->getName();
+            $data[$property_name] = $this->$property_name;
+        }
+        return $data;
     }
 }
