21221b305972f32b676695cf351acf749216ba6e
ng-23 <23grattan@gmail.com>
2023-07-18 08:46:33 -0400

    Minor changes Changed error code to 300

--- src/core/model/exceptions/BadFileUpload.php
+++ src/core/model/exceptions/BadFileUpload.php
@@ -3,7 +3,7 @@
 namespace tots\Exceptions;
 
 class BadFileUploadException extends TotsException {
-    protected $code = 400;
+    protected $code = 300;
 
     protected $message = "Invalid file uploaded";
 }
