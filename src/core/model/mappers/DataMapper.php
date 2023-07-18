988d6fb3a00fb86700dc6ad7ed1ee017faba344f
ng-23 <23grattan@gmail.com>
2023-07-18 09:35:09 -0400

    Minor changes Formatting.

--- src/core/model/mappers/DataMapper.php
+++ src/core/model/mappers/DataMapper.php
@@ -3,13 +3,15 @@
 declare(strict_types=1);
 
 namespace tots\Mappers;
+
 use PDO;
 
-abstract class DataMapper{
+abstract class DataMapper
+{
     protected PDO $db_connection;
 
-    public function __construct(PDO $db_connection) {
+    public function __construct(PDO $db_connection)
+    {
         $this->db_connection = $db_connection;
     }
-
 }
