09c3454b759773be3dd78bddfc293bf1d05c82b4
ng-23 <23grattan@gmail.com>
2023-07-18 08:50:51 -0400

    More error codes Added additional error codes to provide more detail about reason an ID is invalid.

--- src/core/model/exceptions/BadId.php
+++ src/core/model/exceptions/BadId.php
@@ -7,8 +7,20 @@ namespace tots\Exceptions;
  * Could be that it's a bad value (eg -1935)
  * or that it doesn't exist
  */
-class BadIdException extends TotsException {
-    protected $code = 1000;
+class BadIdException extends TotsException
+{
+    /**
+     * see https://stackoverflow.com/questions/5868733/what-do-we-need-the-php-exception-code-for-any-use-case-scenario
+     * Maybe subclass this exception
+     * and create more specific exceptions like UnknownUserId or DuplicateBlogId
+     * instead of using these error codes
+     */
+    public const DEFAULT_CODE = 1000;
+    public const DUPE_ID = 1010;
+    public const UNKNOWN_ID = 1020;
+    public const ID_OUT_OF_RANGE = 1030;
 
-    protected $message = "ID invalid";
+    protected $code = self::DEFAULT_CODE;
+
+    protected $message = "Bad ID";
 }
